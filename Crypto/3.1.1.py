05f1415c6cc9b0fc3298627e0331812ce4262b3e
Hongbo Zheng <hongboz2@illinois.edu>
2022-10-29 20:10:04 -0500

    :white_check_mark: Finish Crypto CP1 3.1.[1,2,3,4,5] :white_check_mark:

--- Crypto/3.1.1.py
+++ Crypto/3.1.1.py
@@ -5,8 +5,9 @@ FILE_HEX_STR = '3.1.1_value.hex'
 def main():
     with open(FILE_HEX_STR, 'r') as f:
         hex_str = f.read().strip()
-        int_ = int(hex_str,16)
-        bin_ = bin(int_)[2:]
+
+    int_ = int(hex_str,16)
+    bin_ = bin(int_)[2:]
 
     print('[HEX_STR]: %s'%hex_str)
     print('[INT]:     %d'%int_)
