69c8c2b6d7e4c6961fb02269635a61a4d9a2413f
Hongbo Zheng <hongboz2@illinois.edu>
2022-10-30 18:15:57 -0500

    :hammer: Add Crypto CP2 3.2.2 scripts :hammer:

--- Crypto/exec_payload.bash
+++ Crypto/exec_payload.bash
@@ -25,6 +25,112 @@ then
 fi
 printf "[INFO]: Finish appending file %s to file %s\n" $PAYLOAD_SUFFIX_FILE $COL2_FILE
 
+printf "\n[INFO]: Check that %s and %s have the same MD5 Hash\n" $GOOD_FILE $EVIL_FILE
+OPENSSL_MD5_GOOD_FILE=$(openssl dgst -md5 $GOOD_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute MD5(%s)\n" $GOOD_FILE
+    exit $RETURN
+fi
+MD5_GOOD_FILE=$(cut -d " " -f 1 <<< $OPENSSL_MD5_GOOD_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute cut %s\n" $OPENSSL_MD5_GOOD_FILE
+    exit $RETURN
+fi
+MD5_HASH_GOOD_FILE=$(cut -d " " -f 2 <<< $OPENSSL_MD5_GOOD_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute cut %s\n" $OPENSSL_MD5_GOOD_FILE
+    exit $RETURN
+fi
+printf "[INFO]: %s %s\n" $MD5_GOOD_FILE $MD5_HASH_GOOD_FILE
+OPENSSL_MD5_EVIL_FILE=$(openssl dgst -md5 $EVIL_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute MD5(%s)\n" $EVIL_FILE
+    exit $RETURN
+fi
+MD5_EVIL_FILE=$(cut -d " " -f 1 <<< $OPENSSL_MD5_EVIL_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute cut %s\n" $OPENSSL_MD5_EVIL_FILE
+    exit $RETURN
+fi
+MD5_HASH_EVIL_FILE=$(cut -d " " -f 2 <<< $OPENSSL_MD5_EVIL_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute cut %s\n" $OPENSSL_MD5_EVIL_FILE
+    exit $RETURN
+fi
+printf "[INFO]: %s %s\n" $MD5_EVIL_FILE $MD5_HASH_EVIL_FILE
+printf "[INFO]: Checking if the MD5 Hash are the same...\n"
+if [ "$MD5_HASH_GOOD_FILE" != "$MD5_HASH_EVIL_FILE" ]
+then
+    printf "[ERROR]: The MD5 Hash of file %s and file %s are different. Quit.\n" $GOOD_FILE $EVIL_FILE
+    exit 1
+fi
+printf "[INFO]: The MD5 Hash of file %s and file %s are the same\n" $GOOD_FILE $EVIL_FILE
+
+printf "\n[INFO]: Check that %s and %s have different SHA256 Hash\n" $GOOD_FILE $EVIL_FILE
+OPENSSL_SHA256_GOOD_FILE=$(openssl dgst -sha256 $GOOD_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute SHA256(%s)\n" $GOOD_FILE
+    exit $RETURN
+fi
+SHA256_GOOD_FILE=$(cut -d " " -f 1 <<< $OPENSSL_SHA256_GOOD_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute cut %s\n" $OPENSSL_SHA256_GOOD_FILE
+    exit $RETURN
+fi
+SHA256_HASH_GOOD_FILE=$(cut -d " " -f 2 <<< $OPENSSL_SHA256_GOOD_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute cut %s\n" $OPENSSL_SHA256_GOOD_FILE
+    exit $RETURN
+fi
+printf "[INFO]: %s %s\n" $SHA256_GOOD_FILE $SHA256_HASH_GOOD_FILE
+OPENSSL_SHA256_EVIL_FILE=$(openssl dgst -sha256 $EVIL_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute SHA256(%s)\n" $GOOD_FILE
+    exit $RETURN
+fi
+SHA256_EVIL_FILE=$(cut -d " " -f 1 <<< $OPENSSL_SHA256_EVIL_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute cut %s\n" $OPENSSL_SHA256_EVIL_FILE
+    exit $RETURN
+fi
+SHA256_HASH_EVIL_FILE=$(cut -d " " -f 2 <<< $OPENSSL_SHA256_EVIL_FILE)
+RETURN=$?
+if [ $RETURN -ne 0 ]
+then
+    printf "[ERROR]: Failed to execute cut %s\n" $OPENSSL_SHA256_EVIL_FILE
+    exit $RETURN
+fi
+printf "[INFO]: %s %s\n" $SHA256_EVIL_FILE $SHA256_HASH_EVIL_FILE
+printf "[INFO]: Checking if the SHA256 Hash are different...\n"
+if [ "$SHA256_HASH_GOOD_FILE" == "$SHA256_HASH_EVIL_FILE" ]
+then
+    printf "[ERROR]: The SHA256 Hash of file %s and file %s are different. Quit.\n" $GOOD_FILE $EVIL_FILE
+    exit 1
+fi
+printf "[INFO]: The SHA256 Hash of file %s and file %s are different\n" $GOOD_FILE $EVIL_FILE
+
 printf "\n[INFO]: Check that %s and %s generate different outputs\n" $GOOD_FILE $EVIL_FILE
 printf "[INFO]: %s generate " $GOOD_FILE
 python3 $GOOD_FILE
