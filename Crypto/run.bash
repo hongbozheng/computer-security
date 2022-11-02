55ac83c07138955d5aae2f42300f5582aec13794
Hongbo Zheng <hongboz2@illinois.edu>
2022-11-02 16:59:07 -0500

    :bug: Add Crypto CP2 3.2.5 :bug: :bug: :bug:

--- Crypto/run.bash
+++ Crypto/run.bash
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
-GET_CERT_PREFIX="get_cert_prefix.py"
+GET_CERT_COL="get_cert_col.py"
 CERT_FILE="cert.cer"
 CERT_PREFIX_FILE="cert_prefix"
 FASTCOLL="fastcoll"
 FASTCOLL_DIR="fastcoll_v1.0.0.5-1_source/"
-MD5_1_FILE="md5_1"
-MD5_2_FILE="md5_2"
+CERT_COL1_FILE="cert_col1"
+CERT_COL2_FILE="cert_col2"
 
 printf "[INFO]: Get %s file from %s...\n" $CERT_PREFIX_FILE $CERT_FILE
-./$GET_CERT_PREFIX $CERT_FILE $CERT_PREFIX_FILE $FASTCOLL_DIR$FASTCOLL $MD5_1_FILE $MD5_2_FILE
+./$GET_CERT_COL $CERT_FILE $CERT_PREFIX_FILE $FASTCOLL_DIR$FASTCOLL $CERT_COL1_FILE $CERT_COL2_FILE
 printf "[INFO]: Finish writing into %s file\n" $CERT_PREFIX_FILE
