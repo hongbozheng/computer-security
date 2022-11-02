339a9729f8d516b44854abd0cdb724d9d19781aa
Hongbo Zheng <hongboz2@illinois.edu>
2022-11-02 00:30:05 -0500

    :white_check_mark: Finish Crypto CP2 3.2.3 :white_check_mark:

--- Crypto/sol_3.2.3.py
+++ Crypto/sol_3.2.3.py
@@ -3,92 +3,77 @@
 
 import sys
 import urllib.request,urllib.error
-from binascii import hexlify
 
-BYTE_LEN=16
+HEX_16=0x10
 URL='http://172.22.159.75:8080/mp3/fa22_cs461_hongboz2/?'
 DEBUG=1
-
-
-
-def strip_padding(msg):
-    padlen = 17 - ord(msg[-1])
-    if padlen > 16 or padlen < 1:
-        return True, None
-    if msg[-padlen:] != ''.join(chr(i) for i in range(16,16-padlen,-1)):
-        return True, None
-    return False, msg[:-padlen]
-
-
-# print(len(ciphertext))
-
-# _, msg = strip_padding("".join(result))
 
 def get_status(u):
     try:
         resp = urllib.request.urlopen(u)
-        # if DEBUG: print(resp.read)
         return resp.code
     except urllib.error.HTTPError as e:
         # if DEBUG: print(e, e.code)
         return e.code
 
-# Pad the message to a multiple of 16 bytes
-def pad(msg):
-    mod = len(msg)%BYTE_LEN
-    return msg + ''.join(chr(i) for i in range(BYTE_LEN,mod,-1))
+def get_plaintxt(decrypted_char):
+    decrypted_char.reverse()
+    pad_num = len(decrypted_char)%HEX_16
+    if pad_num == 0:
+        pad_num = HEX_16
+    for i in range(pad_num):
+        del decrypted_char[-1]
+    return ''.join(decrypted_char)
 
 def main():
     if len(sys.argv) != 3:
         print('[USAGE]: ./sol_3.2.3.py <ciphertext.enc.asc> <decrypted_msg_file>')
         exit(1)
 
-    with open(sys.argv[1]) as cipher_file:
-        cipher = bytes.fromhex(cipher_file.read().strip())
-    if DEBUG: print('[CIPHER]: %s'%cipher)
-    exit()
-    char_decrypted = []
+    with open(sys.argv[1]) as ciphertxt_hex_file:
+        ciphertxt_byte = bytearray(bytes.fromhex(ciphertxt_hex_file.read().strip()))
+    if DEBUG: print('[CIPHERTXT_BYTE]: %s\n'%bytes(ciphertxt_byte))
 
-    for pos in range(len(cipher) // 16 - 1, 0, -1):
-        prev_cipher = cipher[16 * (pos - 1):16 * pos]
-        current_block = cipher[16 * pos:16 * (pos + 1)]
-        for offset in range(1, 17):
-            target_byte = prev_cipher[-offset]
-            # print(offset)
+    decrypted_char = []
+
+    if DEBUG: print('[INFO]: Start Decryption...')
+    for pos in range(len(ciphertxt_byte)//HEX_16-1, 0, -1):
+        ciphertxt_byte_prev = ciphertxt_byte[16*(pos-1):16*pos]
+        cur_blk = ciphertxt_byte[HEX_16*pos:HEX_16*(pos+1)]
+        for offset in range(1, HEX_16+1):
+            target_byte = ciphertxt_byte_prev[-offset]
             for guess in range(256):
-                prev_ciphertext[-offset] = guess
+                ciphertxt_byte_prev[-offset] = guess
                 if (guess != target_byte):
-                    fake_cipher = hexlify(prev_ciphertext) + hexlify(current_block)
-                    # print(fake_cipher)
-                    url = URL + fake_cipher.decode()
-                    # print(url)
+                    fake_cipher = ciphertxt_byte_prev.hex() + cur_blk.hex()
+                    url = URL + fake_cipher
                     status = get_status(url)
 
                     if status == 404:
-                        # print(prev_ciphertext)
-                        # print(guess, target_byte)
-                        char_decrypted.append(chr(guess ^ 16 ^ target_byte))
-                        print(chr(guess ^ 16 ^ target_byte))
+                        decrypted_char.append(chr(target_byte^guess^HEX_16))
+                        if DEBUG: print('[CHAR]: %c'%chr(target_byte^guess^HEX_16))
                         for i in range(offset):
-                            prev_ciphertext[-offset + i] = prev_ciphertext[-offset + i] ^ (16 - i) ^ (15 - i)  # remaining bytes of ct
+                            ciphertxt_byte_prev[-offset+i] = ciphertxt_byte_prev[-offset+i]^(HEX_16-i)^(HEX_16-1-i)
                         break
                 else:
                     guess = 0x10
-                    fake_cipher = hexlify(prev_ciphertext) + hexlify(current_block)
-                    # print(fake_cipher)
-                    url = URL + fake_cipher.decode()
-                    # print(url)
+                    fake_cipher = ciphertxt_byte_prev.hex() + cur_blk.hex()
+                    url = URL + fake_cipher
                     status = get_status(url)
-                    # print(guess, target_byte)
+
                     if status == 404:
-                        char_decrypted.append(chr(guess ^ 16 ^ target_byte))
-                        print(chr(guess ^ 16 ^ target_byte))
+                        decrypted_char.append(chr(target_byte^guess^HEX_16))
+                        if DEBUG: print('[CHAR]: %c'%chr(target_byte^guess^HEX_16))
                         for i in range(offset):
-                            prev_ciphertext[-offset + i] = prev_ciphertext[-offset + i] ^ (16 - i) ^ (
-                                        15 - i)  # remaining bytes of ct
+                            ciphertxt_byte_prev[-offset+i] = ciphertxt_byte_prev[-offset+i]^(HEX_16-i)^(HEX_16-1-i)
                         break
-    char_decrypted.reverse()
-    print(char_decrypted)
+
+    if DEBUG: print('[INFO]: Finish Decryption')
+    plaintxt = get_plaintxt(decrypted_char=decrypted_char)
+    if DEBUG: print('[PLAINTXT]: %s'%plaintxt)
+
+    with open(sys.argv[2], 'w') as decrypted_msg_file:
+        decrypted_msg_file.write(plaintxt)
 
 if __name__ == '__main__':
     main()
