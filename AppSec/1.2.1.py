c703cbe7ede03755be2fcfaab1a38f1ff531bdba
Hongbo Zheng <hongboz2@illinois.edu>
2022-12-08 13:35:56 -0600

    :passport_control: chmod +x :passport_control:

--- AppSec/1.2.1.py
+++ AppSec/1.2.1.py
@@ -7,6 +7,6 @@ from struct import pack
 '''directly overwrite grade buffer using name buffer'''
 
 NAME_BUFFER_SIZE = 10
-NETID = 'netid'
+NETID = 'hongboz2'
 
 sys.stdout.buffer.write(NETID.encode('UTF-8')+b'\00'*(NAME_BUFFER_SIZE-len(NETID))+b'A+')
